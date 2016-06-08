a58e2f1fb00d01cc6ce43a44ec7a4a99498cb0c7
Michael Johnson <michael@pcmac.org>
2016-06-08 00:48:39 -0500

    Master detail components finished

---
+++ app/hero-detail.component.ts
@@ -0,0 +1,25 @@
+﻿import {Component, Input} from '@angular/core';
+import { Hero } from './hero';
+
+@Component({
+    selector: 'hero-detail',
+    template: `
+        <div *ngIf="hero">
+            <h2>Selection Details</h2>
+            <div>
+                <label>Hero Id:</label>
+                {{hero.id}}
+            </div>
+            <div>
+                <label>Hero Name:</label>
+                <input [(ngModel)]="hero.name" placeholder="Hero Name"/>
+            </div>
+        </div>
+    `
+})
+
+
+export class HeroDetailComponent {
+    @Input()
+    hero: Hero;
+}
